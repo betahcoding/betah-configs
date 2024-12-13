de32ebc017050a3023618a9320e89557f781b7f2
Betah Coding <betahcoding@gmail.com>
2024-12-13 08:55:09 +0700

    Remove turbo things from eslint to make it more general

--- packages/eslint-config/base.js
+++ packages/eslint-config/base.js
@@ -1,6 +1,5 @@
 import js from "@eslint/js";
 import eslintConfigPrettier from "eslint-config-prettier";
-import turboPlugin from "eslint-plugin-turbo";
 import tseslint from "typescript-eslint";
 import onlyWarn from "eslint-plugin-only-warn";
 
@@ -14,12 +13,8 @@ export const baseConfig = [
   eslintConfigPrettier,
   ...tseslint.configs.recommended,
   {
-    plugins: {
-      turbo: turboPlugin,
-    },
-    rules: {
-      "turbo/no-undeclared-env-vars": "warn",
-    },
+    plugins: {},
+    rules: {},
   },
   {
     plugins: {
