b2c347ab93b87470f87fa13ed92abe364165af20
Betah Coding <betahcoding@gmail.com>
2024-12-22 11:24:33 +0700

    Update nextjs config description

--- packages/eslint-config/nextjs.mjs
+++ packages/eslint-config/nextjs.mjs
@@ -7,7 +7,7 @@ import pluginNext from '@next/eslint-plugin-next';
 import { baseConfig } from './base.mjs';
 
 /**
- * A custom ESLint configuration for libraries that use Next.js.
+ * A custom flat ESLint configuration for libraries that use Next.js.
  *
  * @type {import("eslint").Linter.Config}
  * */
